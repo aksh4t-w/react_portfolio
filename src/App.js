ff47e4efba1efea4f83a8437a521d5b00eb7ea3c
Akshat <akshatwagadre007@gmail.com>
2021-01-25 02:18:57 +0530

    Final v1.1 (Not mobile responsive)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
-import { CssBaseline } from '@material-ui/core';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
 import { skills } from './myData';
